178a3331d90d5256f21d34cb49ca4ae64a325717
Shashwat Varshney <shashwat.varshney@comviva.com>
2020-05-15 16:17:09 +0530

    third

--- app/src/main/java/com/example/food/orderdetails.java
+++ app/src/main/java/com/example/food/orderdetails.java
@@ -19,7 +19,8 @@ import java.util.List;
 public class orderdetails extends AppCompatActivity implements View.OnClickListener {
     TextView foodView,priceView,textView,viewAddress;
 SharedPreferences sp;
-Button add,order;
+Button add,order,logout;
+TextView f1,f2,f3;
 boolean hasAddress=false;
 
     @Override
@@ -29,22 +30,38 @@ boolean hasAddress=false;
 
         add=(Button)findViewById(R.id.address);
         add.setOnClickListener(this);
+        logout=findViewById(R.id.Logout);
+        logout.setOnClickListener(this);
         order=findViewById(R.id.order);
         order.setOnClickListener(this);
         viewAddress=findViewById(R.id.viewAddress);
         foodView=(TextView) findViewById(R.id.food);
+        f1=findViewById(R.id.food1);
+        f2=findViewById(R.id.food2);
         sp=getSharedPreferences("User",0);
         Long phone=sp.getLong("Phone",0);
         priceView=(TextView) findViewById(R.id.price);
         List<Order> o=MainActivity.useData.myDao().getOrders(phone);
-        String name="";
+        String name="",food1="",food2="",prices="",price1="",price2="";
         Double price=0.0;
+        int i=o.size();
+
         for(Order order:o){
-         name=name+"\n"+order.getItemName();
+           name=name+"\n"+order.getItemName();
          price+=order.getPrice();
         }
-        foodView.setText(name);
-        priceView.setText(price+"");
+        String str[]=name.split("\n");
+
+//Toast.makeText(this,str[0]+str[1]+str[2]+str[3],Toast.LENGTH_LONG).show();
+//        food1=
+        food1=str[1];//name[o.size()-1];
+        food2=str[2];
+        String food3=str[3];
+//        String food3=name[2];
+        foodView.setText(food1);
+        f1.setText(food2);
+        f2.setText(food3);
+        priceView.setText("Total Amount:"+price+"");
         hasAddress=MainActivity.useData.myDao().hasAddress(phone);
         if(hasAddress){
             List<userAddress> ad=MainActivity.useData.myDao().getAddress(phone);
@@ -55,7 +72,7 @@ boolean hasAddress=false;
             for(userAddress address: ad){
                 area+=address.getArea();
                 city+=address.getCity();
-                house=address.getPhone().toString();
+                house=address.getHouseno()+"";
                 pin=address.getPinCode();
             }
             viewAddress.setText("Address is\n"+"#"+house+","+area+","+city+","+pin);
@@ -81,6 +98,11 @@ boolean hasAddress=false;
             case R.id.order:
                 Toast.makeText(this,"Ordered successfuly",Toast.LENGTH_LONG).show();
                 startActivity(new Intent(this,FoodOrder.class));
+                break;
+            case R.id.Logout:
+                Intent i=new Intent(this,MainActivity.class);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(i);
         }
 
 
